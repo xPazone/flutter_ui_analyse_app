c34afd5ee9c4b5bddaa2350a6946c2c7cad84958
xPazone <roman@klimeschhome.de>
2019-11-05 14:07:26 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/de/hsfulda/flutter_ui_analyse_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.hsfulda.flutter_ui_analyse_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
